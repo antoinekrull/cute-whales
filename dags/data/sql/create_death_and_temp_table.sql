a4e6bfd488cb57cffff36b63ed262898a898833a
Antoine Krull <a.krull@campus.lmu.de>
2023-12-06 13:49:47 +0100

    created directory for sql data

---
+++ dags/data/sql/create_death_and_temp_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS deaths_and_temperature (
+                            year CHAR,
+                            month CHAR,
+                            region CHAR,
+                            "total deaths" INTEGER,
+                            temperature DOUBLE PRECISION
+                        );
