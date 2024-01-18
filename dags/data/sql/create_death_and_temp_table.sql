3b9d2e17d0f3142a225123dc340196b1916939bb
Antoine Krull <a.krull@campus.lmu.de>
2024-01-18 18:28:20 +0100

    changed column data types

--- dags/data/sql/create_death_and_temp_table.sql
+++ dags/data/sql/create_death_and_temp_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS deaths_and_temperature (
-                            year CHAR,
-                            month CHAR,
-                            region CHAR,
-                            totaldeaths CHAR,
+                            year VARCHAR,
+                            month VARCHAR,
+                            region VARCHAR,
+                            totaldeaths VARCHAR,
                             temperature DOUBLE PRECISION
                         );
